6d6d1dff1133e23b9d1a5f454097b3cbbbc92fa9
Admer <49600504+Admer456@users.noreply.github.com>
2023-04-18 15:22:44 +0200

    Add aliases for the migrated types

--- src/Globals.cs
+++ src/Globals.cs
@@ -10,3 +10,8 @@ global using Aabb = Godot.Aabb;
 global using Vector2 = Godot.Vector2;
 global using Vector3 = Godot.Vector3;
 global using Vector4 = Godot.Vector4;
+
+global using BrushMapBrush = Elegy.Assets.BrushMapData.Brush;
+global using BrushMapFace = Elegy.Assets.BrushMapData.Face;
+global using BrushMapEntity = Elegy.Assets.BrushMapData.Entity;
+global using BrushMapMaterial = Elegy.Assets.BrushMapData.Material;
